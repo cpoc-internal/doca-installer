8a48b38dfe1ab784297b8de8b1fae0fcc223c3d1
earguedast <este.at@gmail.com>
2026-04-22 22:04:57 -0600

    Create doca-845-amd.sh

---
+++ doca-845-amd.sh
@@ -0,0 +1,18 @@
+wget https://www.mellanox.com/downloads/DOCA/DOCA_v3.3.0/host/doca-host_3.3.0-088000-26.01-ubuntu2204_amd64.deb
+sudo dpkg -i doca-host_3.3.0-088000-26.01-ubuntu2204_amd64.deb
+sudo apt-get update
+sudo apt-get -y install doca-all
+
+sudo apt-get install linux-headers-$(uname -r)
+sudo systemctl enable rshim
+sudo systemctl start rshim
+
+sudo systemctl status doca-telemetry
+# Or for the DPU management
+sudo systemctl status rshim
+
+sudo mst start
+sudo mst status -v
+
+ibv_devinfo
+lsmod | grep ib_peermem
