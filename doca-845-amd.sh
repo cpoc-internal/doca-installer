17b86e25454ff8f36f7f182a2435380d7aeade4a
earguedast <este.at@gmail.com>
2026-04-22 22:10:33 -0600

    Update doca-845-amd.sh

--- doca-845-amd.sh
+++ doca-845-amd.sh
@@ -1,9 +1,10 @@
+sudo apt install -y linux-headers-$(uname -r)
+sudo apt install -y build-essential
 wget https://www.mellanox.com/downloads/DOCA/DOCA_v3.3.0/host/doca-host_3.3.0-088000-26.01-ubuntu2204_amd64.deb
 sudo dpkg -i doca-host_3.3.0-088000-26.01-ubuntu2204_amd64.deb
 sudo apt-get update
 sudo apt-get -y install doca-all
 
-sudo apt-get install linux-headers-$(uname -r)
 sudo systemctl enable rshim
 sudo systemctl start rshim
 
