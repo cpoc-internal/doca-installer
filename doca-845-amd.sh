a294f71f59371eca2f41056a4352523fbcb06cdb
earguedast <este.at@gmail.com>
2026-04-23 10:00:52 -0600

    Update doca-845-amd.sh

--- doca-845-amd.sh
+++ doca-845-amd.sh
@@ -1,5 +1,7 @@
 sudo apt install -y linux-headers-$(uname -r)
 sudo apt install -y build-essential
+sudo apt update
+sudo apt install -y linux-tools-$(uname -r)
 wget https://www.mellanox.com/downloads/DOCA/DOCA_v3.3.0/host/doca-host_3.3.0-088000-26.01-ubuntu2204_amd64.deb
 sudo dpkg -i doca-host_3.3.0-088000-26.01-ubuntu2204_amd64.deb
 sudo apt-get update
@@ -14,3 +16,4 @@ sudo mst status -v
 
 ibv_devinfo
 lsmod | grep ib_peermem
+sudo cpupower frequency-set -g performance
